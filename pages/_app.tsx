e94f173541b260b9665f5f1a7c1bc1e72be0170f
residentevil0803 <ramseyklassen0803@gmail.com>
2021-05-26 10:42:33 +0200

    add graphql

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import "../styles/globals.css";
+import { Provider as StyletronProvider } from 'styletron-react'
+import { styletron } from '../Styletron.js'
+import { useApollo } from '../lib/apollo'
+
+import { ApolloProvider } from '@apollo/client'
+import { LightTheme, BaseProvider } from 'baseui';
+
+function MyApp({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps.initialApolloState)
+
+  return (
+    <StyletronProvider value={styletron}>
+      <ApolloProvider client={apolloClient}>
+        <BaseProvider theme={LightTheme}>
+          <Component {...pageProps} />
+        </BaseProvider>
+      </ApolloProvider >
+    </StyletronProvider>
+  );
+}
+
+export default MyApp;
