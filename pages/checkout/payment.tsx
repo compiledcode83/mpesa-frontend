10f5f39e6ff365f8999042b5588ae9e27fad9492
residentevil0803 <ramseyklassen0803@gmail.com>
2021-06-02 10:38:48 +0200

    update text

--- pages/checkout/payment.tsx
+++ pages/checkout/payment.tsx
@@ -41,7 +41,7 @@ export default function Payment() {
                     Order successfull
                 </StyledTitle>
                 <StyledBody>
-                    Order was succesfull open the app to
+                    To proceed select your payment plan in the app.
                 </StyledBody>
                 <StyledAction>
                     <Button onClick={() => router.push('https://sevi.io/app')} overrides={{ BaseButton: { style: { width: '100%' } } }}>
