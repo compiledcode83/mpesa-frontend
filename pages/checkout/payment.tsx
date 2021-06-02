d6e3b1db322a64e47901b2308019535d9240b329
residentevil0803 <ramseyklassen0803@gmail.com>
2021-06-02 10:35:13 +0200

    adding payment page

---
+++ pages/checkout/payment.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Button } from 'baseui/button';
+import { useRouter } from "next/router";
+import {
+    Card,
+    StyledBody,
+    StyledAction,
+    StyledHeaderImage,
+    StyledTitle
+} from 'baseui/card';
+import { useStyletron } from 'baseui';
+
+export default function Payment() {
+    const [css] = useStyletron();
+    const router = useRouter();
+
+
+    return (
+        <div style={{
+            display: "flex",
+            justifyContent: 'center',
+            marginTop: 100
+
+        }}>
+            <Card
+                overrides={{ Root: { style: { width: '500px' } } }}
+            >
+                <div style={{
+                    position: "relative",
+                    // top: "50%",
+                    left: "40%"
+                }}>
+                    <StyledHeaderImage
+                        className={css({
+                            width: '100px',
+                        })}
+                        src='/images/seviS.svg'
+                    />
+                </div>
+                <StyledTitle overrides={{ BaseButton: { style: { width: '100%' } } }}>
+                    Order successfull
+                </StyledTitle>
+                <StyledBody>
+                    Order was succesfull open the app to
+                </StyledBody>
+                <StyledAction>
+                    <Button onClick={() => router.push('https://sevi.io/app')} overrides={{ BaseButton: { style: { width: '100%' } } }}>
+                        OPEN SEVI APP
+        </Button>
+                </StyledAction>
+            </Card>
+        </ div>
+    );
+}
