e0acad299a0aad0713fbdbbc7a97fe79c4275d12
residentevil0803 <ramseyklassen0803@gmail.com>
2021-09-30 14:48:30 +0200

    fix: reenable button remove image

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -15,21 +15,21 @@ const Banner = () => {
           Buy products and pay for them later
         </p>
         <Button
-          // className={styles.bannerButton}
+          className={styles.bannerButton}
           onClick={() => {router.push('https://sevi.io/app') }}
           endEnhancer={() => <ChevronRight size={24} />}
         >
           INSTALL APP
         </Button>
       </div>
-      <div className={styles.bannerRight}>
+      {/* <div className={styles.bannerRight}>
         <span className={styles.bannerImage1}>
           <Image src="/images/mobile1.svg" alt="" width={520} height={950} />
         </span>
         <span className={styles.bannerImage2}>
           <Image src="/images/mobile1.svg" alt="" width={490} height={870} />
         </span>
-      </div>
+      </div> */}
     </div>
   );
 };
