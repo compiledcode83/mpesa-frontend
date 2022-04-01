56380b7ec85425656d82cfe9922347d2f4c1fbc9
residentevil0803 <ramseyklassen0803@gmail.com>
2022-04-01 15:17:41 +0200

    Update sendFileToBackend.ts

--- helpers/sendFileToBackend.ts
+++ helpers/sendFileToBackend.ts
@@ -10,7 +10,7 @@ export const sendFileToBackend = async (files: any) => {
     
         const query  = {
           method: "POST",
-          mode:  "no-cors", // 'cors' by default
+        //   mode:  "no-cors", // 'cors' by default
           body,
           // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
           // credentials: 'same-origin', // include, *same-origin, omit
